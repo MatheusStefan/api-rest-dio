de7577663b05ec3f7c8186436702710fbf0d3a5d
MatheusStefan <roccomatheus0@gmail.com>
2022-10-10 08:24:18 -0300

    Publicar usuário

--- src/main/java/me/dio/com/api/controller/UsuarioController.java
+++ src/main/java/me/dio/com/api/controller/UsuarioController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import me.dio.com.api.model.Usuario;
@@ -32,5 +35,10 @@ public class UsuarioController {
 	@DeleteMapping(value = "/{id}")
 	public void delete(@PathVariable("id") Integer id) {
 		repository.remove(id);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public void postUser(@RequestBody Usuario usuario) {
+		repository.save(usuario);
 	}
 }
