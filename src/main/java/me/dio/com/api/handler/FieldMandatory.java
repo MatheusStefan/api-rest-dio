436885c78e8758cdc6faa2aa0abcaa9a9466937f
MatheusStefan <roccomatheus0@gmail.com>
2022-10-10 11:06:30 -0300

    Alterações finais

--- src/main/java/me/dio/com/api/handler/FieldMandatory.java
+++ src/main/java/me/dio/com/api/handler/FieldMandatory.java
@@ -1,6 +1,8 @@
 package me.dio.com.api.handler;
 
 public class FieldMandatory extends BusinessException {
+	private static final long serialVersionUID = 1L;
+
 public FieldMandatory(String campo) {
 	super("O campo %s é obrigatório", campo);
 }
--- src/main/java/me/dio/com/api/repository/UsuarioRepository.java
+++ src/main/java/me/dio/com/api/repository/UsuarioRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
-import me.dio.com.api.handler.BusinessException;
 import me.dio.com.api.handler.FieldMandatory;
 import me.dio.com.api.model.Usuario;
 
