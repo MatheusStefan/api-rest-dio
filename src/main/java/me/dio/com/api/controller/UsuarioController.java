8626361c368e4c11040d537425ba732c62647f34
MatheusStefan <roccomatheus0@gmail.com>
2022-10-10 08:17:43 -0300

    Deleção de usuário

--- src/main/java/me/dio/com/api/controller/UsuarioController.java
+++ src/main/java/me/dio/com/api/controller/UsuarioController.java
@@ -3,26 +3,34 @@ package me.dio.com.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import me.dio.com.api.model.Usuario;
 import me.dio.com.api.repository.UsuarioRepository;
 
 @RestController
+@RequestMapping(value = "/users")
 public class UsuarioController {
 
 	@Autowired
 	private UsuarioRepository repository;
 	
-	@GetMapping(value = "/users")
+	@GetMapping
 	public List<Usuario> getUsers() {
 		return repository.listAll(); 
 	}
 	
-	@GetMapping(value = "/user/{username}")
+	@GetMapping(value = "/{username}")
 	public Usuario getUser(@PathVariable("username") String username) {
 		return repository.findByUsername(username);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public void delete(@PathVariable("id") Integer id) {
+		repository.remove(id);
 	}
 }
